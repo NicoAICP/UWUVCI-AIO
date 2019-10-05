7175b7dabbb691cfa73f55d940309142f7889316
NicoAICP <nicohaberkorn@gmx.de>
2019-10-05 17:33:21 +0200

    Updated Credits

--- UWUVCI AIO/Credits.Designer.cs
+++ UWUVCI AIO/Credits.Designer.cs
@@ -40,6 +40,7 @@
             this.label1.Size = new System.Drawing.Size(202, 338);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Credits
             // 
